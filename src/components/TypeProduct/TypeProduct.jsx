278690914afffae1ac4c17c2de7718e6ecaf493b
bùi văn vũ <143469624+buivanvu038@users.noreply.github.com>
2023-12-27 12:07:07 +0700

    Update UI end

--- src/components/TypeProduct/TypeProduct.jsx
+++ src/components/TypeProduct/TypeProduct.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Fragment } from "react";
+import NavBarComponent from "../../components/NavBarComponent/NavBarComponent";
+import CardComponent from "../../components/CardComponent/CardComponent";
+import { Col, Pagination, Row } from "antd";
+import { WrapperNavbar, WrapperProducts } from "./style";
 
-const TypeProduct = ({ name }) => {
-  return <div>{name}</div>;
+const TypeProductPage = () => {
+  const onChange = () => {};
+  return (
+    <div style={{ padding: "0 120px", background: "#efefef" }}>
+      <Row style={{ flexWrap: "nowrap", paddingTop: "10px" }}>
+        <WrapperNavbar span={4}>
+          <NavBarComponent />
+        </WrapperNavbar>
+        <Col span={20}>
+          <WrapperProducts>
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+          </WrapperProducts>
+          <Pagination
+            defaultCurrent={2}
+            total={100}
+            onChange={onChange}
+            style={{ textAlign: "center", marginTop: "10px" }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
-export default TypeProduct;
+export default TypeProductPage;
